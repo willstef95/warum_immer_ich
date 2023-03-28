c22ebd227b1efbd0197dd70497794a329cebcc96
hannesbrugger <hannes.brugger.007@gmail.com>
2023-03-28 16:54:07 +0200

    removed trash

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,15 +2,6 @@
   println("Herzlich Willkommen bei dem Spiel wii")
   print(ausgaben())
 
-// val eol = sys.props("line.separator")
-// def PgrenzeO_U(b: Int = 2) = "---------" * b + eol
-// def PgrenzeL_R(b: Int = 2) = "|       |" * 2 + eol
-// def PgrenzeL_RZ(b: Int = 2) = "|   X   |" * 2 + eol
-// def PgrenzeL_RO(b: Int = 2) = "|   O   |" * 2 + eol
-// def feld() = PgrenzeL_R() + PgrenzeL_RO() + PgrenzeL_RZ() + PgrenzeL_R()
-// def feldO() = PgrenzeL_R() + PgrenzeL_RO() + PgrenzeL_RZ() + PgrenzeL_R()
-// def ausgaben(b: Int = 2) =
-//   (PgrenzeO_U(b) + feld() + PgrenzeO_U(b) + feld() + PgrenzeO_U(b))
 val eol = sys.props("line.separator")
 def PgrenzeO_U(b: Int = 2) = "---------" * b + eol
 def PgrenzeL_R(b: Int = 2) = "|       |"
