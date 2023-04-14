fcc0e1663ca0ffeab0ebd6d1b969237f1617dbd8
willstef95 <stefan-willmann95@web.de>
2023-04-14 08:50:10 +0200

    createFeld

--- src/main/scala/de.htwg.se.wii/model/Field.scala
+++ src/main/scala/de.htwg.se.wii/model/Field.scala
@@ -1,21 +1,18 @@
 package de.htwg.se.wii.model
 
-enum Hole:
-  case Full, Empty
-
-case class Field(rows: Vector[Vector[Hole]]):
-  def clear(): Field = copy()
-  def holestat() = print("Is in process")
-  // override def toString(): String = {
-
+case class Field(cellNumero: Int):
   def eol = sys.props("line.separator")
-  def PgrenzeO_U(b: Int = 2) = "---------" * b + eol
-  def PgrenzeL_R(b: Int = 2) = "|       |"
-  def PgrenzeL_RZ(b: Int = 20) = "|   " + zahl() + "   |"
-  def PgrenzeL_RO(b: Int = 2) = "|   O   |"
-  def feldLeer(b: Int = 2) = PgrenzeL_R() * b + eol
-  def feldO(b: Int = 2) = PgrenzeL_RO() * b + eol
-  def feldX(b: Int = 2) = PgrenzeL_RZ() * b + eol
-  def zahl(b: Int = 0) = "x"
-  def feld(b: Int = 2) = feldLeer(b) + feldO(b) + feldX(b) + feldLeer(b)
-  def ausgaben(b: Int = 2) = ((PgrenzeO_U(b) + feld(b)) * b + PgrenzeO_U(b))
+  // defines the bar
+  def bar(cellWidth: Int = 3, cellNum: Int = 3) =
+    (("+" + "-" * cellWidth) * cellNum) + "+" + endl
+  // defines the cells
+  def cells(cellWidth: Int = 3, cellNum: Int = 3) =
+    ("|" + " " * cellWidth) * cellNum + "|" + endl
+  // defines the grid and default size is 10x10 field
+  def mesh(cellWidth: Int = 3, cellNum: Int = cellNumero) =
+    (bar(cellWidth, cellNum) + cells(cellWidth, cellNum)) * cellNum + bar(
+      cellWidth,
+      cellNum
+    )
+  // prints grid default size is 10x10
+  println(mesh())
