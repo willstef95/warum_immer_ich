6d8d473860c236b814022b6c0919a555862ba1c1
willstef95 <stefan-willmann95@web.de>
2023-05-12 12:39:24 +0200

    ...

--- src/main/scala/de.htwg.se.wii/util/Observable.scala
+++ src/main/scala/de.htwg.se.wii/util/Observable.scala
@@ -5,6 +5,7 @@ trait Observer:
 
 trait Observable:
   var subscribers: Vector[Observer] = Vector()
-  def add(s: Observer) = subscribers = subscribers :+ s
-  def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
-  def notifyObservers = subscribers.foreach(o => o.update)
+  def add(s: Observer): Unit = subscribers = subscribers :+ s
+  def remove(s: Observer): Unit = subscribers =
+    subscribers.filterNot(o => o == s)
+  def notifyObservers(): Unit = subscribers.foreach(o => o.update)
