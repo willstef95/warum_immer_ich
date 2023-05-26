bbf797a16f904c087efd301602ec9bc14843bf0b
hannesbrugger <hannes.brugger.007@gmail.com>
2023-05-26 12:12:10 +0200

    PutO Spec

--- src/test/scala/de.htwg.se.wii/controller/PutOCommandSpec.scala
+++ src/test/scala/de.htwg.se.wii/controller/PutOCommandSpec.scala
@@ -11,6 +11,7 @@ import de.htwg.se.wii.controller.Controller
 import de.htwg.se.wii.model.Game
 import de.htwg.se.wii.model.Dice
 import de.htwg.se.wii.util.Stat
+import de.htwg.se.wii.util.Command
 
 class PutOCommandSpec extends AnyWordSpec {
   "A PutOCommandSpec" when {}
@@ -18,55 +19,37 @@ class PutOCommandSpec extends AnyWordSpec {
     val field = new Field()
     val controller = Controller(field, 3)
     var game = new Game(("Spieler1", "Spieler2"), 2, 2)
-    val command1 = new PutXCommand(controller, Hole(HoleX, 3))
-    command1.doStep(field)
     val command = new PutOCommand(controller, Hole(HoleO, 3))
+    val command1 = new PutXCommand(controller, Hole(HoleX, 3))
+    controller.doAndPublish(controller.putO, Hole(HoleO, 3))
+    controller.doAndPublish(controller.putX, Hole(HoleX, 3))
+
+    controller.undoManager.doStep(
+      field,
+      PutXCommand(controller, Hole(HoleO, 3))
+    )
+    controller.undoStep()
+
+    command1.doStep(field)
 
     "when doStep" in {
-      command.doStep(field) should be(("""
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #""").stripMargin('#'))
-      controller.game.pens1 should be(3)
+      undoManager.undoStep(field)
+      controller.get(3) should be(HoleO)
+      controller.game.pens2 should be(3)
     }
     "when noStep" in {
-      command.noStep(field) should be(("""
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #""").stripMargin('#'))
+      command.noStep(field)
+      controller.get(3) should be(HoleO)
+      controller.game.pens1 should be(3)
     }
     "when undoStep" in {
-      command.undoStep(field) should be(("""
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| X | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #""").stripMargin('#'))
+      command.undoStep(field)
+      controller.get(3) should be(HoleX)
       controller.game.pens1 should be(2)
     }
     "when redoStep" in {
-      command.redoStep(field) should be(("""
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #| O | O | O |
-          #+---+---+---+
-          #""").stripMargin('#'))
+      command.redoStep(field)
+      controller.get(3) should be(HoleO)
       controller.game.pens1 should be(3)
     }
   }
