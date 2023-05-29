7d3f6559ebd71a181a58f5609cd245ed68649621
hannesbrugger <hannes.brugger.007@gmail.com>
2023-05-29 18:27:03 +0200

    GUI in main

--- src/main/scala/de.htwg.se.wii/Main.scala
+++ src/main/scala/de.htwg.se.wii/Main.scala
@@ -8,7 +8,7 @@ import model.holes.*
 import scala.util.Random
 import scala.annotation.switch
 import scala.io.StdIn.readLine
-import de.htwg.se.wii.aview.{GameUI, TUI}
+import de.htwg.se.wii.aview.{GameUI, TUI, GUI}
 
 @main def run(): Unit =
   println("Herzlich Willkommen")
@@ -16,6 +16,8 @@ import de.htwg.se.wii.aview.{GameUI, TUI}
   //   "Wie gross soll das Spielfeld sein? 2x2,3x3,4x4,5x5... Bitte geben Sie eine Zahl ein"
   // )
   // val size = readLine().toInt
+  val GUI = new ScalaFXHelloWorld()
+  GUI.start()
   val size = 3
   val field = new Field()
   val controller = Controller(field, size)
