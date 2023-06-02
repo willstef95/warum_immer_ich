f75877b3a0bd3aca549d89f5aa4a71388faeec94
hannesbrugger <hannes.brugger.007@gmail.com>
2023-06-02 12:49:28 +0200

    first GUI test

--- src/ObservableSpec.scala
+++ src/ObservableSpec.scala
@@ -1,4 +1,4 @@
-package de.htwg.se.wii.model
+/*package de.htwg.se.wii.model
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
@@ -17,3 +17,4 @@ class ObserverSpec extends AnyWordSpec {
     }
   }
 }
+ */
--- src/main/scala/de.htwg.se.wii/aview/GUI.scala
+++ src/main/scala/de.htwg.se.wii/aview/GUI.scala
@@ -13,51 +13,48 @@ import de.htwg.se.wii.aview.GameUI
 import controller.Controller
 import util.Observer
 import scala.io.StdIn.readLine
+import de.htwg.se.wii.model.holes.HoleState
+import scala.swing.Reactions.Reaction
 
-case class GUI(controller: Controller, size: Int)
-    extends SimpleSwingApplication,
-      GameUI,
-      Observer {
+class GUI(controller: Controller) extends Frame with Observer:
 
+  def cells = new CellPanel(3, 3)
   controller.add(this)
-
-  val eol = sys.props("line.separator")
-  var processInputReturn = true
-
-  override def run(): Unit = {
-    println("Los geht das Spiel")
-    controller.init(init())
-    this.top
-    gameLoop()
-  }
-
-  def init(): (String, String) = {
-    println("Name Spieler 1:")
-    val Player1 = readLine
-    println("Name Spieler 2:")
-    val Player2 = readLine
-    (Player1, Player2)
-  }
-
-  def gameLoop(): Unit = {
-    gameLoop()
-  }
-  override def update: Unit = ???
-
-  def top = new MainFrame {
-    title = "Hello world"
-
-    contents = new FlowPanel {
-      contents += new Label("Launch rainbows:")
-      contents += new Button("Click me") {
-        reactions += { case event.ButtonClicked(_) =>
-          println("All the colours!")
-        }
-      }
+  title = "Wii"
+  menuBar = new MenuBar {
+    contents += new Menu("File") {
+      contents += new MenuItem(Action("Exit") {
+        sys.exit(0)
+      })
     }
-
-    pack()
-    centerOnScreen()
-    open()
   }
-}
+  contents = updateContents
+  pack()
+  centerOnScreen()
+  open()
+
+  def updateContents = {
+    new BorderPanel {
+      add(
+        new Label("Player: " + controller.game.names),
+        BorderPanel.Position.North
+      )
+      add(cells, BorderPanel.Position.Center)
+    }
+  }
+
+  override def update: Unit =
+    contents = updateContents
+
+  class CellPanel(x: Int, y: Int) extends GridPanel(x, y):
+    (for (
+      x <- 0 to 2;
+      y <- 0 to 2
+    ) yield (x, y, controller.field.matrix.cell(x, y))).foreach(t =>
+      contents += new CellButton(t._1, t._2, t._3)
+    )
+
+  def button(stone: String) = new Button(stone)
+
+  class CellButton(x: Int, y: Int, hole: HoleState)
+      extends Button(hole.toString())
